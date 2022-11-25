94ba3dd717d2f93fec8ca92560aa3da2268ef07a
bek2518 <bereketb20@hotmail.com>
2022-11-25 11:33:45 +0300

    Console with entry point ti the command interpreter

--- console.py
+++ console.py
@@ -1,8 +1,45 @@
 #!/usr/bin/python3
+'''
+Program that contains an entry point of
+the command interpreter
+'''
 import cmd
 
-class HBNBCommand(cmd.cmd):
+class HBNBCommand(cmd.Cmd):
+    '''
+    class definition
+    '''
+    prompt = '(hbnb)'
 
+    def do_EOF(self, arg):
+        '''
+        Quit the program when EOF
+        '''
+        return True
+    
+    def do_quit(self, arg):
+        '''
+        Quit the program when quit passed
+        '''
+        return True
 
-#if __name__ == '__main__':
-#    HBNBCommand().cmdloop()
+    def emptyline(self):
+        '''
+        Handles when an empty line is passed
+        '''
+        pass
+
+    def help_EOF(self):
+        '''
+        Defines the help for EOF
+        '''
+        print ('EOF command to exit the program\n')
+    
+    def help_quit(self):
+        '''
+        Defines the help for quit command
+        '''
+        print ('Quit command to exit the program\n')
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
