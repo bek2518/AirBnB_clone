97b59ce3a0cf568eafe863b4facb2aaff127a317
bek2518 <bereketb20@hotmail.com>
2022-11-27 15:03:19 +0300

    Added User class

--- models/__init__.py
+++ models/__init__.py
@@ -2,11 +2,14 @@
 '''
 __init__file
 '''
+
 from models.base_model import BaseModel
 from models.engine.file_storage import FileStorage
+from models.user import User
 
 class_list = {
-    'BaseModel': BaseModel
+    'BaseModel': BaseModel,
+    'User': User
 }
 storage = FileStorage()
 storage.reload()
